7480a9d08d0102a56d9149c37efa7fbbc50555ba
SHREEPRASAD1996 <111556063+SHREEPRASAD1996@users.noreply.github.com>
2022-08-21 23:23:48 -0700

    Add files via upload

---
+++ IncrementDecrementQuiz_4.java
@@ -0,0 +1,14 @@
+package com.anudip;
+
+public class IncrementDecrementQuiz_4 
+{
+	
+	 public static void main(String[] args) 
+	    {
+	        int a=1;
+	         
+	        a = a++ + ++a * --a - a--;
+	         
+	        System.out.println(a);
+    }
+}
---
+++ IncrementDecrementQuiz_5.java
@@ -0,0 +1,13 @@
+package com.anudip;
+
+	public class IncrementDecrementQuiz_5
+	{
+	    public static void main(String[] args) 
+	    {
+	        int m=1010, n=1010;
+	         
+	        System.out.println(m++ / ++n * n-- / --m); 
+	    }
+	}
+
+
